ecba48c873d3c19d02ff4488abbde61589dc79a9
Thomas Schoeps <tom.schoeps@gmail.com>
2020-04-03 14:57:40 +0200

    Use 8 directions for pathfinding, with node expansion according to Jump Point Search (JPS) to avoid redundant expansions

--- src/FreeAge/server/game.cpp
+++ src/FreeAge/server/game.cpp
@@ -3,6 +3,7 @@
 #include <queue>
 
 #include <QApplication>
+#include <QImage>
 #include <QThread>
 
 #include "FreeAge/common/logging.hpp"
@@ -857,7 +858,7 @@ void Game::SimulateGameStepForUnit(u32 unitId, ServerUnit* unit, double gameStep
 void Game::PlanUnitPath(ServerUnit* unit) {
   Timer pathPlanningTimer;
   
-  typedef int CostT;
+  typedef float CostT;
   
   int mapWidth = map->GetWidth();
   int mapHeight = map->GetHeight();
@@ -914,57 +915,191 @@ void Game::PlanUnitPath(ServerUnit* unit) {
   priorityQueue.emplace(start, 0.f);
   
   std::vector<CostT> costSoFar(mapWidth * mapHeight);
-  memset(costSoFar.data(), 0xff, mapWidth * mapHeight * sizeof(CostT));
+  for (int i = 0, end = mapWidth * mapHeight; i < end; ++ i) {
+    costSoFar[i] = std::numeric_limits<float>::infinity();
+  }
   costSoFar[start.x() + mapWidth * start.y()] = 0;
   
+  // Directions are encoded as row-major indices of grid cells in a 4x4 grid,
+  // with (1, 1) being the origin of movement. A 3x3 grid would suffice,
+  // however, with a 4x4 grid, computations are faster. So, for example,
+  // the value 0 corresponds to cell (0, 0) in the grid, which has an offset of (-1, -1)
+  // from the movement origin. So, the movement came from (-1, -1).
+  // Second example: The value 4 corresponds to cell (0, 1) with movement (-1, 0).
+  // The value 5 corresponds to zero movement, this is used for the start and for initialization.
   std::vector<u8> cameFrom(mapWidth * mapHeight);
-  memset(cameFrom.data(), 255, mapWidth * mapHeight);
+  memset(cameFrom.data(), 5, mapWidth * mapHeight);
   
   CostT smallestReachedHeuristicValue = std::numeric_limits<CostT>::max();
   QPoint smallestReachedHeuristicTile(-1, -1);
   
+  static int numNeighborsToCheckArray[11] = {
+    3,
+    7,
+    3,
+    0,  // no valid direction
+    7,
+    8,
+    7,
+    0,  // no valid direction
+    3,
+    7,
+    3,
+  };
+  static QPoint neighborsToCheckArray[11][8] = {
+    {QPoint(1, 0), QPoint(0, 1), /*dependent on previous being free*/ QPoint(1, 1)},
+    {QPoint(0, 1), /*occ. test*/ QPoint(1, -1), QPoint(1, 0), QPoint(1, 1), /*occ. test*/ QPoint(-1, -1), QPoint(-1, 0), QPoint(-1, 1)},
+    {QPoint(-1, 0), QPoint(0, 1), /*dependent on previous being free*/ QPoint(-1, 1)},
+    {},
+    {QPoint(1, 0), /*occ. test*/ QPoint(-1, -1), QPoint(0, -1), QPoint(1, -1), /*occ. test*/ QPoint(-1, 1), QPoint(0, 1), QPoint(1, 1)},
+    {QPoint(0, -1), QPoint(-1, 0), QPoint(1, 0), QPoint(0, 1), QPoint(1, 1), QPoint(-1, -1), QPoint(1, -1), QPoint(-1, 1)},
+    {QPoint(-1, 0), /*occ. test*/ QPoint(1, -1), QPoint(0, -1), QPoint(-1, -1), /*occ. test*/ QPoint(1, 1), QPoint(0, 1), QPoint(-1, 1)},
+    {},
+    {QPoint(0, -1), QPoint(1, 0), /*dependent on previous being free*/ QPoint(1, -1)},
+    {QPoint(0, -1), /*occ. test*/ QPoint(1, 1), QPoint(1, 0), QPoint(1, -1), /*occ. test*/ QPoint(-1, 1), QPoint(-1, 0), QPoint(-1, -1)},
+    {QPoint(-1, 0), QPoint(0, -1), /*dependent on previous being free*/ QPoint(-1, -1)},
+  };
+  // Here we encode that diagonal movements require the two adjacent tiles to be free.
+  static u8 neighborsRequireFreeTiles[11][8] = {
+    {0, 0, 0b11},
+    {0, /*occ. test*/ 0, 0, 0b101, /*occ. test*/ 0, 0, 0b100001},
+    {0, 0, 0b11},
+    {},
+    {0, /*occ. test*/ 0, 0, 0b101, /*occ. test*/ 0, 0, 0b100001},
+    {0, 0, 0, 0, 0b1100, 0b0011, 0b0101, 0b1010},
+    {0, /*occ. test*/ 0, 0, 0b101, /*occ. test*/ 0, 0, 0b100001},
+    {},
+    {0, 0, 0b11},
+    {0, /*occ. test*/ 0, 0, 0b101, /*occ. test*/ 0, 0, 0b100001},
+    {0, 0, 0b11},
+  };
+  
   int debugConsideredNodesCount = 0;
   
+  // Set this to true to have a debug image written to /tmp/FreeAge_pathfinding_debug.png.
+  // Legend:
+  // * Black: Occupied tiles.
+  // * Dark green: open rect.
+  // * White: Free tiles, never considered by pathfinding.
+  // * Light red: Free tiles, considered by pathfinding.
+  // * Light yellow: Free tiles, added to the priority queue as a neighbor but not directly considered.
+  // * Green: Free tiles that are part of the final path.
+  constexpr bool kOutputDebugImage = false;
+  constexpr const char* kDebugImagePath = "/tmp/FreeAge_pathfinding_debug.png";
+  QImage debugImage;
+  if (kOutputDebugImage) {
+    debugImage = QImage(mapWidth, mapHeight, QImage::Format_RGB32);
+    for (int y = 0; y < mapHeight; ++ y) {
+      for (int x = 0; x < mapWidth; ++ x) {
+        if (map->occupiedForUnitsAt(x, y)) {
+          debugImage.setPixel(x, y, qRgb(0, 0, 0));
+        } else {
+          debugImage.setPixel(x, y, qRgb(255, 255, 255));
+        }
+      }
+    }
+    for (int y = openRect.y(); y < openRect.bottom(); ++ y) {
+      for (int x = openRect.x(); x < openRect.right(); ++ x) {
+        debugImage.setPixel(x, y, qRgb(0, 100, 0));
+      }
+    }
+  }
+  
   while (!priorityQueue.empty()) {
-    ++ debugConsideredNodesCount;
-    
     Location current = priorityQueue.top();
     priorityQueue.pop();
+    
+    ++ debugConsideredNodesCount;
+    if (kOutputDebugImage) {
+      debugImage.setPixel(current.loc.x(), current.loc.y(), qRgb(255, 127, 127));
+    }
     
     if (current.loc == goal) {
       break;
     }
     
-    CostT currentCost = costSoFar[current.loc.x() + mapWidth * current.loc.y()];
+    int currentGridIndex = current.loc.x() + mapWidth * current.loc.y();
+    CostT currentCost = costSoFar[currentGridIndex];
+    int currentCameFrom = cameFrom[currentGridIndex];
     
-    // TODO: Use 8 neighbors, including diagonal movements, instead of 4.
-    // TODO: Can we use JPS' expansion strategy? (Skip looking at some neighbors
-    //       for which we know that the cost will not decrease.)
-    for (int neighborIdx = 0; neighborIdx < 4; ++ neighborIdx) {
-      QPoint nextTile(
-          current.loc.x() + ((neighborIdx == 0) ? -1 : ((neighborIdx == 1) ? 1 : 0)),
-          current.loc.y() + ((neighborIdx == 2) ? -1 : ((neighborIdx == 3) ? 1 : 0)));
+    int numNeighborsToCheck = numNeighborsToCheckArray[currentCameFrom];
+    QPoint* neighbors = neighborsToCheckArray[currentCameFrom];
+    u8 freeNeighbors = 0;  // bitmask with a 1 for each free neighbor
+    
+    for (int neighborIdx = 0; neighborIdx < numNeighborsToCheck; ++ neighborIdx) {
+      QPoint neighborDir = neighbors[neighborIdx];
+      QPoint nextTile = current.loc + neighborDir;
       int nextGridIndex = nextTile.x() + mapWidth * nextTile.y();
       
+      // Special case:
+      // For straight movements, numNeighborsToCheck is 7. It contains two sets of directions preceded by an occupancy check.
+      // This means that we only consider those directions if the occupancy-checked tile is occupied.
+      // The occupancy check is at neighbor indices 1 and 4 and each applies to the two following neighbors.
+      int skipLenght = 0;
+      if (numNeighborsToCheck == 7 &&
+          (neighborIdx == 1 || neighborIdx == 4)) {
+        // This neighbor is an occupancy check. If it is occupied, we need to consider the following
+        // two neighbors, otherwise we can skip them.
+        skipLenght = 2;
+      } else {
+        u8 requiredFreeNeighbors = neighborsRequireFreeTiles[currentCameFrom][neighborIdx];
+        if ((freeNeighbors & requiredFreeNeighbors) != requiredFreeNeighbors) {
+          // The required previous free neighbors are not free.
+          continue;
+        }
+      }
+      
+      // Skip neighbor if it is outside of the map.
       if (nextTile.x() < 0 || nextTile.y() < 0 ||
-          nextTile.x() >= mapWidth || nextTile.y() >= mapHeight ||
-          (map->occupiedForUnitsAt(nextTile.x(), nextTile.y()) && !openRect.contains(nextTile, false))) {
+          nextTile.x() >= mapWidth || nextTile.y() >= mapHeight) {
+        neighborIdx += skipLenght;
+        continue;
+      }
+      // Skip neighbor if it is occupied.
+      if (map->occupiedForUnitsAt(nextTile.x(), nextTile.y()) && !openRect.contains(nextTile, false)) {
+        // Continue while not skipping over possible neighbors depending on this as an occupancy check (since the check returned true).
         continue;
       }
       
-      CostT newCost = currentCost + 1;
+      freeNeighbors |= 1 << neighborIdx;
+      
+      // Skip this if it is a failed occupancy check (instead of an actual neighbor).
+      if (skipLenght > 0) {
+        // If this was an occupancy check, but the tile is free, continue while skipping over the dependent neighbors.
+        neighborIdx += skipLenght;
+        continue;
+      }
+      
+      // Compute the cost to reach this neighbor from the start.
+      constexpr float sqrt2 = 1.41421356237310f;
+      CostT newCost = currentCost + ((neighborDir.manhattanLength() == 2) ? sqrt2 : 1);
+      
+      // If the cost is better than the best cost known so far, expand the path to this neighbor.
       CostT* nextCostSoFar = &costSoFar[nextGridIndex];
-      if (*nextCostSoFar == -1 || newCost < *nextCostSoFar) {
+      if (newCost < *nextCostSoFar) {
         *nextCostSoFar = newCost;
         
-        CostT heuristic = std::abs(nextTile.x() - goal.x()) + std::abs(nextTile.y() - goal.y());
+        // Compute the "diagonal distance" as a heuristic for the remaining path length to the goal.
+        // This is a distance metric on the grid while allowing diagonal movements.
+        int xDiff = std::abs(nextTile.x() - goal.x());
+        int yDiff = std::abs(nextTile.y() - goal.y());
+        int minDiff = std::min(xDiff, yDiff);
+        int maxDiff = std::max(xDiff, yDiff);
+        CostT heuristic = minDiff * sqrt2 + (maxDiff - minDiff) * 1;
+        
+        // Remember the closest tile to the goal that we found. This becomes important
+        // in case we cannot reach the goal at all.
         if (heuristic < smallestReachedHeuristicValue) {
           smallestReachedHeuristicValue = heuristic;
           smallestReachedHeuristicTile = nextTile;
         }
         
         priorityQueue.emplace(nextTile, newCost + heuristic);
-        cameFrom[nextGridIndex] = neighborIdx;
+        cameFrom[nextGridIndex] = ((-neighborDir.x()) + 1) + 4 * ((-neighborDir.y()) + 1);
+        
+        if (kOutputDebugImage) {
+          debugImage.setPixel(nextTile.x(), nextTile.y(), qRgb(255, 255, 127));
+        }
       }
     }
   }
@@ -1002,19 +1137,27 @@ void Game::PlanUnitPath(ServerUnit* unit) {
   while (currentTile != start) {
     reversePath.push_back(QPointF(currentTile.x() + 0.5f, currentTile.y() + 0.5f));
     
+    if (kOutputDebugImage) {
+      debugImage.setPixel(currentTile.x(), currentTile.y(), qRgb(0, 255, 0));
+    }
+    
     int cameFromDirection = cameFrom[currentTile.x() + mapWidth * currentTile.y()];
-    if (cameFromDirection == 0) {
-      currentTile.setX(currentTile.x() + 1);
-    } else if (cameFromDirection == 1) {
-      currentTile.setX(currentTile.x() - 1);
-    } else if (cameFromDirection == 2) {
-      currentTile.setY(currentTile.y() + 1);
-    } else if (cameFromDirection == 3) {
-      currentTile.setY(currentTile.y() - 1);
-    } else {
-      LOG(ERROR) << "Unexpected value in cameFrom[] while reconstructing path: " << cameFromDirection;
+    if (cameFromDirection >= 11 || numNeighborsToCheckArray[cameFromDirection] == 0 || cameFromDirection == 5) {
+      LOG(ERROR) << "Erroneous value in cameFrom[] while reconstructing path: " << cameFromDirection;
       break;
     }
+    
+    int cameFromX = (cameFromDirection % 4) - 1;
+    currentTile.setX(currentTile.x() + cameFromX);
+    
+    int cameFromY = (cameFromDirection / 4) - 1;
+    currentTile.setY(currentTile.y() + cameFromY);
+  }
+  if (kOutputDebugImage) {
+    debugImage.setPixel(start.x(), start.y(), qRgb(0, 255, 0));
+    
+    LOG(WARNING) << "Writing pathfinding debug image to: " << kDebugImagePath;
+    debugImage.save(kDebugImagePath);
   }
   
   // Replace the last point with the exact goal location (if we can reach the goal)
@@ -1031,7 +1174,7 @@ void Game::PlanUnitPath(ServerUnit* unit) {
   // Smooth the planned path by attempting to drop corners.
   // TODO
   
-  LOG(1) << "Pathfinding: Took " << pathPlanningTimer.Stop(false) << " s";
+  LOG(1) << "Pathfinding: Took " << pathPlanningTimer.Stop(false) << " s" << (kOutputDebugImage ? " (not accurate since kOutputDebugImage is true!)" : "");
   
   // Assign the path to the unit.
   unit->SetPath(reversePath);
