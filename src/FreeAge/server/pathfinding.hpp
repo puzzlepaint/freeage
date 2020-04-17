10d8a123b7115ad9e6afbdb9aa251da139372381
Thomas Schoeps <tom.schoeps@gmail.com>
2020-04-17 14:59:44 +0200

    Refactoring: Put the pathfinding code on the server into separate files pathfinding.cpp/hpp to reduce the size of the Game class

---
+++ src/FreeAge/server/pathfinding.hpp
@@ -0,0 +1,10 @@
+// Copyright 2020 The FreeAge authors
+// This file is part of FreeAge, licensed under the new BSD license.
+// See the COPYING file in the project root for the license text.
+
+#pragma once
+
+class ServerMap;
+class ServerUnit;
+
+void PlanUnitPath(ServerUnit* unit, ServerMap* map);
